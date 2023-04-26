7e487061431693fff5af5ad156d0f4a068111658
khanchandaniaakash <131808721+khanchandaniaakash@users.noreply.github.com>
2023-04-26 14:15:09 +0530

    Update streamlit_app.py
    added breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Masala Dosa')
 streamlit.text('Vada Sambhar')
+streamlit.text('Idli Sambhar')
+streamlit.text('uttapam')
